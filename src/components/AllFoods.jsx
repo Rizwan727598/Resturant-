19f03a9424b799c39c3cbe1372e8fe0ce9fbea5b
Rizwan727598 <rizwanislam369@gmail.com>
2025-01-20 14:19:50 +0600

    text change

--- src/components/AllFoods.jsx
+++ src/components/AllFoods.jsx
@@ -55,7 +55,7 @@ const AllFoods = () => {
             placeholder="Search by name"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="px-5 py-2 border border-gray-300 dark:border-gray-600 rounded-lg mr-2 bg-gray-50 dark:bg-gray-700 text-gray-800 dark:text-gray-300"
+            className="px-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg mr-2 bg-gray-50 dark:bg-gray-700 text-gray-800 dark:text-gray-300"
           />
           <button
             type="submit"
